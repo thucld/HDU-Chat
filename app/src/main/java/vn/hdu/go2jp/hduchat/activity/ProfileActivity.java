cf6b73d1290c40a18fc7baa2750fec207b50490d
thucld <thucld.it@gmail.com>
2018-07-06 17:01:59 +0700

    add profile activity

---
+++ app/src/main/java/vn/hdu/go2jp/hduchat/activity/ProfileActivity.java
@@ -0,0 +1,15 @@
+package vn.hdu.go2jp.hduchat.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vn.hdu.go2jp.hduchat.R;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
