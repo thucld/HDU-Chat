6f8edc121c56787ae73751d71d98e196a7af8af7
thucld <thucld.it@gmail.com>
2018-07-07 11:28:44 +0700

    add More Feature fragment

--- app/src/main/java/vn/hdu/go2jp/hduchat/fragment/MoreFragment.java
+++ app/src/main/java/vn/hdu/go2jp/hduchat/fragment/MoreFragment.java
@@ -1,17 +1,22 @@
 package vn.hdu.go2jp.hduchat.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import vn.hdu.go2jp.hduchat.R;
+import vn.hdu.go2jp.hduchat.activity.ProfileActivity;
 
 /**
  * Where to show more features.
  */
 public class MoreFragment extends Fragment {
+
+    FrameLayout btnProfile;
 
     public MoreFragment() {
         // Required empty public constructor
@@ -21,7 +26,15 @@ public class MoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_more, container, false);
+        View view = inflater.inflate(R.layout.fragment_more, container, false);
+
+        btnProfile = view.findViewById(R.id.btnProfile);
+        btnProfile.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), ProfileActivity.class);
+            startActivity(intent);
+        });
+
+        return view;
     }
 
 }
