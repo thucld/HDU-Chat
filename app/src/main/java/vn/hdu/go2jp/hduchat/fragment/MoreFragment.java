469046a1dd3fa2e137400d72c2bb1148e7a987d5
thucld <thucld.it@gmail.com>
2018-07-12 15:38:06 +0700

    load user avatar more fragment

--- app/src/main/java/vn/hdu/go2jp/hduchat/fragment/MoreFragment.java
+++ app/src/main/java/vn/hdu/go2jp/hduchat/fragment/MoreFragment.java
@@ -2,14 +2,19 @@ package vn.hdu.go2jp.hduchat.fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
+import com.bumptech.glide.Glide;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 import vn.hdu.go2jp.hduchat.R;
 import vn.hdu.go2jp.hduchat.activity.ProfileActivity;
+import vn.hdu.go2jp.hduchat.util.FireBaseUtil;
 
 /**
  * Where to show more features.
@@ -17,24 +22,42 @@ import vn.hdu.go2jp.hduchat.activity.ProfileActivity;
 public class MoreFragment extends Fragment {
 
     FrameLayout btnProfile;
+    CircleImageView civProfile;
 
     public MoreFragment() {
         // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_more, container, false);
+        initViews(view);
+        getData();
+        setupEvents();
+        return view;
+    }
 
+    private void initViews(View view) {
+        civProfile = view.findViewById(R.id.civProfile);
         btnProfile = view.findViewById(R.id.btnProfile);
+    }
+
+    private void getData() {
+        FireBaseUtil.getInstance().getThisUser(user ->
+                Glide.with(this.getActivity())
+                        .load(user.getAvatarPath())
+                        .error(R.drawable.img_user_avatar_default_3)
+                        .fitCenter()
+                        .into(civProfile));
+    }
+
+    private void setupEvents() {
         btnProfile.setOnClickListener(v -> {
             Intent intent = new Intent(getActivity(), ProfileActivity.class);
             startActivity(intent);
         });
-
-        return view;
     }
 
 }
